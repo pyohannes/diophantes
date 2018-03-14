36fd4fe3e670d84c97e0259c45e58aac154b8485
Johannes Tax <johannes@johannes.tax>
2018-03-14 17:36:11 +0100

    Fix web site layout.
     - Change font to Robot
     - Add a help section
     - Add an about section
     - Fix style
    
    Fixes #83, fixes #82, fixes #23.

--- web/dioweb.rkt
+++ web/dioweb.rkt
@@ -16,7 +16,7 @@
 (define (<header>)
   (list 'head
         (<icon> "diophantus_icon.png")
-        `(title ,(make-cdata #f #f "&Delta;iophantus"))
+        `(title "Diophantus")
         `(script ((type "text/javascript"))
            ,(make-cdata #f #f "
               function diowebOpen(formula) {
@@ -44,14 +44,159 @@
   (define formula (formula-from-command-line))
   (define formulaoutput
     (with-handlers ([exn:fail? <errormessage>])
-      (if (non-empty-string? formula)
-          (<formulaoutput> formula)
-          '())))
+      (cond ((equal? formula "about")
+             (<about>))
+            ((equal? formula "help")
+             (<help>))
+            ((non-empty-string? formula)
+             (<formulaoutput> formula))
+            (else
+              '()))))
   (list-non-null
     'body
+    `(span ((class "header"))
+       (a ((class "base navbar")
+           (href "javascript:diowebOpen('about');"))
+          "About")
+       (a ((class "base navbar")
+           (href "javascript:diowebOpen('help');"))
+          "Help"))
     `(h1 ,(make-cdata #f #f "&Delta;iophantus"))
     (<formulainput> formula)
     formulaoutput))
+
+(define (<about>)
+  (define a-racket  '(a ((class "base")
+                         (href "https://www.racket-lang.org")) "Racket"))
+  (define a-mathjax '(a ((class "base")
+                         (href "https://www.mathjax.org")) "MathJax"))
+  (define a-function-plot
+                    '(a ((class "base")
+                         (href "https://mauriciopoppe.github.io/function-plot")) 
+                       "function-plot"))
+  (define a-roboto  '(a ((class "base")
+                         (href "https://github.com/google/roboto")) "Roboto"))
+  `(div ((class "textflow"))
+    (h2 "About")
+    (p
+      "Diophantus is a CGI executable written in " ,a-racket ". It serves as "
+      "a web frontend for the racket-symalg library.")
+    (p 
+      "racket-symalg is a Racket library which I developed as a pet project "
+      "of mine. It allows one to express and manipulate algebraic expressions "
+      "in Racket.")
+    (p
+      "Diophantus makes use of:"
+      (ul
+        (li ,a-mathjax " to display formulas,")
+        (li ,a-function-plot " to plot functions of one variable and")
+        (li "the " ,a-roboto " font.")))
+    (p
+      "All source code can be found on github:"
+      (ul
+        (li (a ((class "base")
+                (href "https://github.com/pyohannes/racket-symalg"))
+               "racket-symalg"))
+        (li (a ((class "base")
+                (href "https://github.com/pyohannes/diophantus"))
+               "diophantus"))))
+    (p
+      "You can contact the author at "
+      (a ((class "base")
+          (href "mailto:johannes@johannes.tax"))
+         "johannes@johannes.tax")
+      ".")))
+
+(define (<help>)
+  `(div ((class "textflow"))
+    (h2 "Help")
+    (p
+      "Diophantus is supposed to give derivatives and plots for arbitrary "
+      "elementary functions. Currently supported operations are:"
+      (ul
+        (li "arithmetic operations \\(+ - \\times \\div\\)")
+        (li "exponentials")
+        (li "logarithms")
+        (li "the constants \\(\\pi\\) and \\( e \\)")
+        (li "the trigonometric functions \\(\\sin\\), \\(\\cos\\) and "
+            "\\(\\tan\\)")))
+    (p
+      "A function can be entered in the input field as infix or as "
+      "s-expression, on confirmation the function is simplified, plotted "
+      "and the derivative is calculated.")
+    (p
+      "The following table shows what operations can be entered. Infix and "
+      "s-expressions must not be mixed.")
+    (table ((class "base help"))
+      (tr ((class "header"))
+        (td "Operation")
+        (td "infix")
+        (td "sexpr"))
+      (tr
+        (td "\\(x + 1\\)")
+        (td "x + 1")
+        (td "(+ x 1)"))
+      (tr
+        (td "\\(x - 1\\)")
+        (td "x - 1")
+        (td "(- x 1)"))
+      (tr
+        (td "\\(x \\times 1\\)")
+        (td "x * 1")
+        (td "(* x 1)"))
+      (tr
+        (td "\\(\\frac{1}{x}\\)")
+        (td "1/x")
+        (td "(/ 1 x)"))
+      (tr
+        (td "\\(x^2\\)")
+        (td "x^2")
+        (td "(expt x 2)"))
+      (tr
+        (td "\\( \\ln(x) \\)")
+        (td "ln(x)")
+        (td "(ln x)"))
+      (tr
+        (td "\\( \\log(x) \\)")
+        (td "log(x)")
+        (td "(log x)"))
+      (tr
+        (td "\\( \\pi \\)")
+        (td "pi")
+        (td "pi"))
+      (tr
+        (td "\\( e \\)")
+        (td "e")
+        (td "e"))
+      (tr
+        (td "\\( \\sin(x) \\)")
+        (td "sin(x)")
+        (td "(sin x)"))
+      (tr
+        (td "\\( \\cos(x) \\)")
+        (td "cos(x)")
+        (td "(cos x)"))
+      (tr
+        (td "\\( \\tan(x) \\)")
+        (td "tan(x)")
+        (td "(tan x)")))
+  (p
+    "In the expressions above \\( x \\) can be replaced by any other "
+    "expression. For example, entering ")
+  (p ((class "formula"))
+    "(e^tan(x))/(1 - x^2)")
+  (p
+    "yields the function")
+  (p "$$\\frac{e^{tan(x)}}{1 - x^2}$$")
+  (p
+    "This software is still work in progress and may give incorrect and "
+    "suboptimal results in some cases. In case you have any problems, "
+    "questions or suggestions, please contact "
+    (a ((class "base")
+        (href "mailto:johannes@johannes.tax"))
+       "johannes@johannes.tax")
+    ".")
+  ))
 
 (define (<errormessage> e)
   `(p ((class "error"))
@@ -71,7 +216,7 @@
   (define f/dexpr-simple (simplify f/dexpr))
   (define f/dexpr-deriv (simplify (differentiate f/dexpr-simple)))
   (list-non-null
-    'table
+    'table '((class "base main"))
     (<caption/tablerow> "Input")
     (<formula/tablerow> formula)
     (<caption/tablerow> "Formula")
@@ -103,6 +248,8 @@
      (script ((type "text/javascript"))
        ,(make-cdata #f #f (format "
            functionPlot({
+               width: 550,
+               height: 350,
                target: '#plot',
                data: [{
                    fn: '~a',
@@ -111,10 +258,8 @@
                }]
            })" (infix f #t))))))
 
-(define (<img/png> data)
-  (sleep 100)
-  `(img ((src ,(string-append "data:image/png;base64,"
-                              data)))))
+(define (<img> url)
+  `(img ((src ,url))))
 
 ; -----------------
 ; Utility functions
@@ -143,8 +288,7 @@
     (string-append "javascript:diowebOpen('"
                    (~a (infix dexpr))
                    "');"))
-  `(a ((href ,href)
-       (class "formula"))
+  `(a ((href ,href))
       ,formula))
 
 (define (parse s)
